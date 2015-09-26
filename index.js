de3d36d842bade6dd3b5db118580861b48d86f38
FranciscoP <franciscopresencia@hotmail.com>
2015-09-26 20:11:04 +0200

    Removed a line

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // Simply remove the trailing slash
 module.exports = function(req, res, next) {
   if(req.url.length > 1 && req.url.substr(-1) == '/') {
-    res.redirect(301, req.url.slice(0, -1));
+    return res.redirect(301, req.url.slice(0, -1));
   }
-  else {
-    next();
-  }
+  next();
 };
